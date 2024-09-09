fbc5b9d285fada27e1490d89d27f723dacde07ad
lourenztansiongco <lourenztansiongco@gmail.com>
2024-09-09 08:45:04 +0800

    not finished dbase error int

--- database/migrations/2024_09_07_072943_create_tasks_table.php
+++ database/migrations/2024_09_07_072943_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->longText('description');
+            $table->longText('description')->nullable();
             $table->string('image_path')->nullable();
             $table->string('status');
             $table->string('priority');
